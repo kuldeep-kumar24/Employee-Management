697d1ca81abd248af92f6fd8d3b79d78a0ddf7a4
kuldeep yadav <ky5167520@gmail.com>
2025-10-28 16:50:25 +0530

    "Add Dockerfile for Tomcat deployment"

--- src/main/java/com/employee/Main.java
+++ src/main/java/com/employee/Main.java
@@ -1,12 +1,18 @@
 package com.employee;
 import org.apache.catalina.startup.Tomcat;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(Integer.parseInt(System.getenv().getOrDefault("PORT", "8080")));
         tomcat.getHost().setAppBase(".");
-        tomcat.addWebapp("/", "src/main/webapp/");
+        File webappDir = new File("webapp");
+        if (!webappDir.exists()) {
+            webappDir = new File("target/classes/webapp");
+        }
+        tomcat.addWebapp("/", webappDir.getAbsolutePath());
         System.out.println("Starting embedded Tomcat...");
         tomcat.start();
         tomcat.getServer().await();
